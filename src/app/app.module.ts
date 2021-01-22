403400fa96f83b9e290652622b4b49a77213a4ca
xUser5000 <abdallah.11306@stemmenof.moe.edu.eg>
2021-01-22 10:19:08 +0200

    Set up angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { UiModule } from './ui/ui.module';
 import { AppComponent } from './app.component';
+import { ServiceModule } from './service/service.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    UiModule,
+    ServiceModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
