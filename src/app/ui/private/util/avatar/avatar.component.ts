2cb7b40b36c5ec681c9a94b50fccd49bfa2ac390
xUser5000 <abdallah.11306@stemmenof.moe.edu.eg>
2021-01-30 18:48:40 +0200

    Added topbar layout

---
+++ src/app/ui/private/util/avatar/avatar.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.css']
+})
+export class AvatarComponent implements OnInit {
+
+  @Input() public url: string = "";
+  @Input() public small: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
